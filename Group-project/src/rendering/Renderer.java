ccedb7172ba874dc46c00011736e004bfdff1ff1
kudos3939 <annie1020k@gmail.com>
2014-10-09 20:11:02 +1300

    trying to add shadow mapping but still doesn't work

--- Group-project/src/rendering/Renderer.java
+++ Group-project/src/rendering/Renderer.java
@@ -1,11 +1,9 @@
 package rendering;
 
-import javax.media.opengl.GL;
-import javax.media.opengl.GL2;
-import javax.media.opengl.GLAutoDrawable;
-import javax.media.opengl.GLEventListener;
+import javax.media.opengl.*;
 import javax.media.opengl.glu.GLU;
 
+import com.jogamp.opengl.util.gl2.GLUT;
 import rendering.primitive.*;
 import ui.Board;
 
@@ -31,8 +29,14 @@ public class Renderer implements GLEventListener, KeyListener {
     private Vector3D lookAt = new Vector3D(0.0,42.0,0.0);
     private Vector3D cameraTop = new Vector3D(0.0,1.0,0.0);
 
+    private float[] lightPos = new float[]{10f,100f,30f,1f};
+    private float[] lightStr = new float[]{1f,1f,1f,1f};
+
+    double[] modelview = new double[16];
+
     private double angle = 0;
 
+    private int shadowMapTex[] = new int[1];
 //    private Texture texture;
 
     public Renderer(Board board, int uid) {
@@ -50,7 +54,7 @@ public class Renderer implements GLEventListener, KeyListener {
         GL2 gl = drawable.getGL().getGL2();
         glu = GLU.createGLU(gl);
 //        gl.glShadeModel(GL2.GL_SMOOTH);              // Enable Smooth Shading
-        gl.glClearColor(0.0f, 0.0f, 0.0f, 0.5f);    // Black Background
+        gl.glClearColor(0.0f, 0.0f, 1.0f, 0.5f);    // Black Background
 //        gl.glClearDepth(1.0f);                      // Depth Buffer Setup
         gl.glEnable(GL.GL_DEPTH_TEST);              // Enables Depth Testing
         gl.glDepthFunc(GL.GL_LEQUAL);               // The Type Of Depth Testing To Do
@@ -59,6 +63,8 @@ public class Renderer implements GLEventListener, KeyListener {
         enableLighting(gl);
         gl.glEnable(GL.GL_CULL_FACE);
         gl.glCullFace(GL.GL_BACK);
+
+        initShadowMap(gl);
         
 //        try {
 //			texture = TextureIO.newTexture(new File("wall.jpg"), false);
@@ -73,8 +79,35 @@ public class Renderer implements GLEventListener, KeyListener {
 //			// TODO Auto-generated catch block
 //			e.printStackTrace();
 //		}
-        
-        
+    }
+
+    private void initShadowMap(GL2 gl) {
+
+        gl.glTexImage2D(GL2.GL_TEXTURE_2D,0,GL2.GL_DEPTH_COMPONENT, width,height,0,GL2.GL_DEPTH_COMPONENT,GL2.GL_UNSIGNED_BYTE,null);
+
+        gl.glTexParameteri(GL.GL_TEXTURE_2D, GL.GL_TEXTURE_MAG_FILTER, GL.GL_NEAREST);
+        gl.glTexParameteri(GL.GL_TEXTURE_2D,GL.GL_TEXTURE_MIN_FILTER,GL.GL_NEAREST);
+
+        gl.glTexParameteri(GL.GL_TEXTURE_2D,GL.GL_TEXTURE_WRAP_S,GL2.GL_CLAMP);
+        gl.glTexParameteri(GL.GL_TEXTURE_2D,GL.GL_TEXTURE_WRAP_T,GL2.GL_CLAMP);
+
+        gl.glTexParameteri(GL.GL_TEXTURE_2D,GL2.GL_TEXTURE_COMPARE_MODE,GL2.GL_COMPARE_R_TO_TEXTURE);
+
+        gl.glTexParameteri(GL.GL_TEXTURE_2D,GL2.GL_TEXTURE_COMPARE_FUNC,GL.GL_LEQUAL);
+
+        gl.glTexParameteri(GL.GL_TEXTURE_2D,GL2.GL_DEPTH_TEXTURE_MODE,GL.GL_LUMINANCE);
+
+        gl.glTexGeni(GL2.GL_S, GL2.GL_TEXTURE_GEN_MODE,GL2.GL_EYE_LINEAR);
+        gl.glTexGeni(GL2.GL_T, GL2.GL_TEXTURE_GEN_MODE,GL2.GL_EYE_LINEAR);
+        gl.glTexGeni(GL2.GL_R, GL2.GL_TEXTURE_GEN_MODE,GL2.GL_EYE_LINEAR);
+        gl.glTexGeni(GL2.GL_Q, GL2.GL_TEXTURE_GEN_MODE,GL2.GL_EYE_LINEAR);
+
+        double[][] gen = new double[][]{{1,0,0,0},{0,1,0,0},{0,0,1,0},{0,0,0,1}};
+
+        gl.glTexGendv(GL2.GL_S,GL2.GL_EYE_PLANE,gen[0],0);
+        gl.glTexGendv(GL2.GL_T,GL2.GL_EYE_PLANE,gen[1],0);
+        gl.glTexGendv(GL2.GL_R,GL2.GL_EYE_PLANE,gen[2],0);
+        gl.glTexGendv(GL2.GL_Q,GL2.GL_EYE_PLANE,gen[3],0);
     }
 
     private void enableLighting(GL2 gl) {
@@ -95,7 +128,7 @@ public class Renderer implements GLEventListener, KeyListener {
     public void display(GLAutoDrawable drawable) {
         GL2 gl = drawable.getGL().getGL2();
         update(gl);
-        render(gl);
+        enableShadowMapping(gl);
     }
 
     @Override
@@ -117,8 +150,7 @@ public class Renderer implements GLEventListener, KeyListener {
 
     private void update(GL2 gl) {
         setCamera(gl);
-        float[] temp = new float[]{10f,100f,30f,1f};
-        gl.glLightfv(GL2.GL_LIGHT0,GL2.GL_POSITION,temp,0);
+        gl.glLightfv(GL2.GL_LIGHT0,GL2.GL_POSITION, lightPos,0);
     }
 
     private void setCamera(GL2 gl) {
@@ -128,9 +160,93 @@ public class Renderer implements GLEventListener, KeyListener {
         gl.glRotated(angle,0,1,0);
     }
 
-    private void render(GL2 gl) {
+    private void enableShadowMapping(GL2 gl) {
+        createDepthBuffer(gl);
+//        applyDepthBuffer(gl);
+        System.out.println("hoge");
+//        gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+//        render(gl);
+    }
 
+    private void createDepthBuffer(GL2 gl) {
+        int[] viewport = new int[4];
+        double[] projection = new double[16];
+
+        gl.glClear(GL.GL_DEPTH_BUFFER_BIT);
+
+        gl.glGetIntegerv(GL.GL_VIEWPORT,viewport,0);
+
+        gl.glViewport(0,0, width,height);
+
+        gl.glGetDoublev(GL2.GL_PROJECTION_MATRIX,projection,0);
+
+        gl.glMatrixMode(GL2.GL_PROJECTION);
+        gl.glLoadIdentity();
+
+        gl.glMatrixMode(GL2.GL_MODELVIEW);
+        gl.glLoadIdentity();
+        glu.gluPerspective(40.0,width/height,1, 300);
+        glu.gluLookAt((double)lightPos[0],(double)lightPos[1],(double)lightPos[2],0,0,0,0,1,0);
+
+        gl.glGetDoublev(GL2.GL_MODELVIEW_MATRIX, modelview,0);
+
+        gl.glColorMask(false,false,false,false);
+
+        gl.glDisable(GL2.GL_LIGHTING);
+
+        gl.glCullFace(GL.GL_FRONT); //##########################
+
+        render(gl);
+
+        gl.glCopyTexSubImage2D(GL.GL_TEXTURE_2D,0,0,0,0,0,width,height);
+
+        gl.glViewport(viewport[0],viewport[1],viewport[2],viewport[3]);
+        gl.glMatrixMode(GL2.GL_PROJECTION);
+        gl.glLoadMatrixd(projection, 0);
+        gl.glColorMask(true, true, true, true);
+        gl.glEnable(GL2.GL_LIGHTING);
+        gl.glCullFace(GL.GL_BACK); //##########################
+
+    }
+
+    private void applyDepthBuffer(GL2 gl) {
         gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
+
+        gl.glMatrixMode(GL2.GL_MODELVIEW);
+        gl.glLoadIdentity();
+
+        gl.glLightfv(GL2.GL_LIGHT0,GL2.GL_POSITION, lightPos,0);
+
+        gl.glMatrixMode(GL2.GL_TEXTURE);
+        gl.glLoadIdentity();
+
+        gl.glTranslated(0.5,0.5,0.5);
+        gl.glScaled(0.5, 0.5, 0.5);
+
+        gl.glMultMatrixd(modelview,0);
+
+        gl.glMatrixMode(GL2.GL_MODELVIEW);
+
+        gl.glEnable(GL.GL_TEXTURE_2D);
+        gl.glEnable(GL2.GL_TEXTURE_GEN_S);
+        gl.glEnable(GL2.GL_TEXTURE_GEN_T);
+        gl.glEnable(GL2.GL_TEXTURE_GEN_R);
+        gl.glEnable(GL2.GL_TEXTURE_GEN_Q);
+
+        gl.glLightfv(GL2.GL_LIGHT0,GL2.GL_DIFFUSE, lightStr,0);
+        gl.glLightfv(GL2.GL_LIGHT0,GL2.GL_SPECULAR, lightStr,0);
+
+        render(gl);
+
+        gl.glDisable(GL2.GL_TEXTURE_GEN_S);
+        gl.glDisable(GL2.GL_TEXTURE_GEN_T);
+        gl.glDisable(GL2.GL_TEXTURE_GEN_R);
+        gl.glDisable(GL2.GL_TEXTURE_GEN_Q);
+        gl.glDisable(GL.GL_TEXTURE_2D);
+
+    }
+
+    private void render(GL2 gl) {
 
 //        board.getRoomContainsPlayer(player).render(gl,texture);
         room.render(gl);
@@ -140,7 +256,9 @@ public class Renderer implements GLEventListener, KeyListener {
         Box.render(gl,new Vector3D(-64,0,0), new Vector3D(20,5,20));
         Tetra.render(gl, new Vector3D(-5, 10, 0), new Vector3D(20, 5, 20),0);
         gl.glFlush();
+
     }
+
 
     @Override
     public void keyTyped(KeyEvent e) {
