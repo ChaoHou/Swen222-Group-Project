d86e03f6baaa4c37272844ae9be6e2fe55f77159
ChaoHou <hch928ls@gmail.com>
2014-10-13 17:45:15 +1300

    Bug need to fix

--- Group-project/src/ui/GameFrame.java
+++ Group-project/src/ui/GameFrame.java
@@ -323,6 +323,7 @@ public class GameFrame extends JFrame {
 		
         public MapScreen getMapPanel(){
         	return this.map;
+//        	return ((GameMenu) this.getPanels().get("game")).getPanels().get("");
         }
 
 		public ActionListener getPlayer() {
